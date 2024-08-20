a30dc0534f101be534f25e68bdaccf8be594663e
Madiocre <wolvirex@gmail.com>
2024-08-20 10:11:21 +0300

    fix 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({ input, output });
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // TODO: Log the name in a database
-  console.log(`Your name is:  ${name}`);
-  rl.close();
-
-  process.on('exit', () => {
-    if (!input.isTTY) {
-      console.log('This important software is now closing');
-    }
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
   });
-});
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
